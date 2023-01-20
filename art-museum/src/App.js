d294891faf9b02cfd06d8964f8bc03ed19f79bc4
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-01-20 16:04:12 -0500

    added stuff

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,6 +1,10 @@
+import harvardArt from "./data/harvardart";
+import GalleryNavigation from "./components/GalleryNavigation";
 function App() {
+  // console.log(harvardArt)
   return (
-    <h1>Hello from App</h1>
+    <GalleryNavigation gallery={`${harvardArt}`}></GalleryNavigation>
+    // <h1>Hello from App</h1>
   );
 }
 
---
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,8 @@
+import harvardArt from "../../data/harvardart";
+
+ const GalleryNavigation = (gallery) => {
+    console.log(gallery)
+    // return <h1>{gallery}</h1>
+}
+
+export default GalleryNavigation;
